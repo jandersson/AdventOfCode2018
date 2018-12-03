f955b2ab7ce507c286d42e7911a30b8d9c75830d
Jonas Andersson <jonas@andersson.io>
2018-12-03 22:44:28 +0100

    fix typo

--- 3/fabric_slicing.py
+++ 3/fabric_slicing.py
@@ -4,12 +4,12 @@ with open('input.txt') as infile:
 
 fabric = [[{'claimed_by': [], 'num_claims': 0} for x in range(1001)] for y in range(1001)]
 claim_re = re.compile("#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)")
-claimaints = list()
+claimants = list()
 for claim in claims:
     match = claim_re.match(claim)
     if match:
         claim_id = int(match.group(1))
-        claimaints.append(claim_id)
+        claimants.append(claim_id)
         claim_pos = (int(match.group(2)), int(match.group(3)))
         claim_dims = (int(match.group(4)), int(match.group(5)))
         for x in range(claim_pos[0], claim_pos[0] + claim_dims[0]):
@@ -23,7 +23,7 @@ for row in fabric:
         if col['num_claims'] > 1:
             num_contested_inches += 1
             for claimaint in col['claimed_by']:
-                if claimaint in claimaints:
-                    claimaints.remove(claimaint)
+                if claimaint in claimants:
+                    claimants.remove(claimaint)
 print(f"Number of square inches with 2 claims or more: {num_contested_inches}")
-print(f"Sole claimaint: {claimaints[0]}")
+print(f"Sole claimant: {claimants[0]}")
