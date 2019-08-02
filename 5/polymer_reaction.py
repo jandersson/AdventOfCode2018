b8ab23f7f481000296a3b207917ee1a432dc9a24
Jonas Andersson <jonas@andersson.io>
2019-08-02 21:51:26 +0200

    Add solution for day 5

--- 5/polymer_reaction.py
+++ 5/polymer_reaction.py
@@ -1,6 +1,6 @@
-test_input = "dabAcCaCBAcCcaDA"
 
 def catalyze(polymer: str) -> str:
+    """Naive implementation"""
     for i, char in enumerate(polymer):
         if i == len(polymer) - 1:
             return polymer
@@ -10,20 +10,41 @@ def catalyze(polymer: str) -> str:
                 return polymer[:i] + polymer[i+2:]
 
 
-def run_catalyzer(polymer):
+def run_catalyzer(polymer: str) -> str:
+    print(f"running catalyzer on {len(polymer)} unit polymer")
     num_iterations = 0
     while True:
+        # TODO: catalyze polymer in one iteration
         num_iterations += 1
         original_polymer = polymer
         polymer = catalyze(original_polymer)
         if polymer == original_polymer:
             print(f"catalyzer took {num_iterations} iterations")
+            print(f"catalyzed polymer is {len(polymer)} units")
             return polymer
 
 
-print(len(run_catalyzer(test_input)))
+test_input = "dabAcCaCBAcCcaDA"
 
 with open('input.txt') as infile:
-    data = infile.readlines()[0]
+    data = infile.readlines()
+    assert len(data) == 1
+    data = data[0].strip()
 
-print(len(run_catalyzer(data)))
+run_catalyzer(test_input)
+# Answer to problem 1
+run_catalyzer(data)
+
+unit_types = ['a', 'b', 'c', 'd']
+for unit in unit_types:
+    print(f"removing unit {unit} from polymer")
+    run_catalyzer(test_input.replace(unit, '').replace(unit.capitalize(), ''))
+
+# Answer to problem 2
+results = {unit: 0 for unit in unit_types}
+for unit in unit_types:
+    print(f"removing unit {unit} from polymer")
+    modified_data = data.replace(unit, '').replace(unit.capitalize(), '')
+    results[unit] = len(run_catalyzer(modified_data))
+min_key = min(results, key=results.get)
+print(f"{min_key} -> {results[min_key]} units")
